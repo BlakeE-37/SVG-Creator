e7e830f31fedd03d40c89e65ae117ecac573f900
Blake H <blake.hollingsworth80@gmail.com>
2023-04-27 11:20:16 -0600

    added the .prompt from inquirer and added questions

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const shape = require('./lib/shape')
+const inquirer = require('inquirer')
 
 //initialize the application
 function init() {
-    return;
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What text would you like on your logo?',
+                name: 'text'
+            },
+            {
+                type: 'input',
+                message: 'What color should the text be?',
+                name: 'textColor'
+            },
+            {
+                type: 'list',
+                message: 'What shape would you like for your logo?',
+                choices: ['Triangle', 'Circle', 'Square'],
+                name: 'shape'
+            },
+            {
+                type: 'input',
+                message: 'What color should the Shape be?',
+                name: 'shapeColor'
+            }
+        ]).then((response) => {
+            console.log(response.text)
+            console.log(response.textColor)
+            console.log(response.shape)
+            console.log(response.shapeColor)
+        })
 }
 
 // application start
